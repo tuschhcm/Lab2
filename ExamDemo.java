3c03e65343e064deece9c79d4500eaac79bea60d
Craig Tuschhoff <tuschhcm@gmail.com>
2013-01-07 16:26:43 -0800

    tabs -> spaces

--- ExamDemo.java
+++ ExamDemo.java
@@ -13,35 +13,35 @@ import java.util.Scanner;
 import java.io.*;
 
 public class ExamDemo {
-	public static void main(String[] args) throws IOException {
-		
-		//create scanner object for user input
-		Scanner keyboard = new Scanner(System.in);
-		char userAnswer;	//stores user input
-		
-		//create true/false exam
-		TrueFalseExam test = new TrueFalseExam(4, "Questions.txt");
-		
-		//loop for all the questions
-		for(int index = 0; index < test.getNumberOfQuestions(); index++) {
-			
-			//ask the question
-			System.out.println(test.getQuestion(index));
-			
-			//get a T or F answer form the user
-			do {
-				userAnswer = keyboard.nextLine().charAt(0);
-				if(userAnswer != 'T' && userAnswer != 'F') {
-					System.out.println("Invalid answer. Please enter T or F");
-				}
-			}while(userAnswer != 'T' && userAnswer != 'F');
-			
-			//check the correctness of the answer
-			if(userAnswer == test.getAnswer(index)) {
-				System.out.println("Correct");
-			} else {
-				System.out.println("Incorrect");
-			}
-		}//end for
-	}//end main
+   public static void main(String[] args) throws IOException {
+      
+      //create scanner object for user input
+      Scanner keyboard = new Scanner(System.in);
+      char userAnswer;  //stores user input
+      
+      //create true/false exam
+      TrueFalseExam test = new TrueFalseExam(4, "Questions.txt");
+      
+      //loop for all the questions
+      for(int index = 0; index < test.getNumberOfQuestions(); index++) {
+         
+         //ask the question
+         System.out.println(test.getQuestion(index));
+         
+         //get a T or F answer form the user
+         do {
+            userAnswer = keyboard.nextLine().charAt(0);
+            if(userAnswer != 'T' && userAnswer != 'F') {
+               System.out.println("Invalid answer. Please enter T or F");
+            }
+         }while(userAnswer != 'T' && userAnswer != 'F');
+         
+         //check the correctness of the answer
+         if(userAnswer == test.getAnswer(index)) {
+            System.out.println("Correct");
+         } else {
+            System.out.println("Incorrect");
+         }
+      }//end for
+   }//end main
 }//end class
--- TrueFalseExam.java
+++ TrueFalseExam.java
@@ -13,51 +13,50 @@ import java.io.*;
 import java.util.Scanner;
 
 public class TrueFalseExam {
-	
-	// instance variables
-	private String[] questions = {"is the earth flat?", "Is a baby beautiful?",
-		"Is water wet?"};
-	private char[] correctAnswers = {'F', 'T', 'T'};
-	
-	// no args constructor
-	public TrueFalseExam() {
-		// do nothing yet
-	}
-	
-	// overloaded constructor, read from file
-	public TrueFalseExam(int numberOfQuestions, String fileName) throws IOException {
-		
-		// 1. Create the File and Scanner objects
-		File file = new File(fileName);
-		Scanner inputFile = new Scanner(file);
-		
-		// 2. Instantiate the two arrays to the correct size
-		questions = new String[numberOfQuestions];
-		correctAnswers = new char[numberOfQuestions];
-		
-		// 3. loop, reading the question and answer data, into the two arrays
-		
-		for(int i = 0; i < numberOfQuestions; i++) {
-			questions[i] = inputFile.nextLine();
-			correctAnswers[i] = inputFile.nextLine().charAt(0);
-		}
-		
-		// 4. Close the Scanner object when your done using it
-		inputFile.close();
-	}
-	
-	// get answer method
-	public char getAnswer(int index) {
-		return correctAnswers[index];
-	}
-	
-	// get question method
-	public String getQuestion(int index) {
-		return questions[index];
-	}
-	
-	//get number of questions method
-	public int getNumberOfQuestions() {
-		return questions.length;
-	}
+   
+   // instance variables
+   private String[] questions = {"is the earth flat?", "Is a baby beautiful?",
+      "Is water wet?"};
+   private char[] correctAnswers = {'F', 'T', 'T'};
+   
+   // no args constructor
+   public TrueFalseExam() {
+      // do nothing yet
+   }
+   
+   // overloaded constructor, read from file
+   public TrueFalseExam(int numberOfQuestions, String fileName) throws IOException {
+      
+      // 1. Create the File and Scanner objects
+      File file = new File(fileName);
+      Scanner inputFile = new Scanner(file);
+      
+      // 2. Instantiate the two arrays to the correct size
+      questions = new String[numberOfQuestions];
+      correctAnswers = new char[numberOfQuestions];
+      
+      // 3. loop, reading the question and answer data, into the two arrays
+      for(int i = 0; i < numberOfQuestions; i++) {
+         questions[i] = inputFile.nextLine();
+         correctAnswers[i] = inputFile.nextLine().charAt(0);
+      }
+      
+      // 4. Close the Scanner object when your done using it
+      inputFile.close();
+   }
+   
+   // get answer method
+   public char getAnswer(int index) {
+      return correctAnswers[index];
+   }
+   
+   // get question method
+   public String getQuestion(int index) {
+      return questions[index];
+   }
+   
+   //get number of questions method
+   public int getNumberOfQuestions() {
+      return questions.length;
+   }
 }
